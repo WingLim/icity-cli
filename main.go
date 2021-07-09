680876241db02de47a3594f1f76a1d9f939179d4
WingLim <winglim@qq.com>
2021-07-09 21:25:07 +0800

    feat: init project

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "icity"
+	app.Version = "0.0.1"
+	app.UseShortOptionHandling = true
+
+	app.Commands = []*cli.Command{}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
